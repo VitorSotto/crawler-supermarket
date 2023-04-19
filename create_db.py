1f7bbbca518d0b55012323dab00587ec1c26d0ec
VitorSotto <vitor.sotto.rodrigues@gmail.com>
2023-04-19 20:49:46 -0300

    Update: create_db.py implement table precos

--- create_db.py
+++ create_db.py
@@ -5,17 +5,27 @@ conn = sqlite3.connect('crawler.db')
 cursor = conn.cursor()
 
 # criando a tabela (schema)
-cursor.execute("""
-CREATE TABLE produtos (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+
+# Cria a tabela "produtos"
+conn.execute('''CREATE TABLE produtos (
+        id_produto TEXT NOT NULL PRIMARY KEY,
         nome TEXT NOT NULL,
-        categoria TEXT NOT NULL,
         fornecedor TEXT,
         mercado TEXT NOT NULL,
         imagem TEXT NOT NULL,
-        preco DECIMAL NOT NULL
-);
-""")
+        id_preco TEXT NOT NULL UNIQUE,
+        FOREIGN KEY (id_preco) REFERENCES precos(id_preco)
+)''')
+
+# Cria a tabela "precos"
+conn.execute('''CREATE TABLE precos (
+        id TEXT NOT NULL PRIMARY KEY,
+        categoria TEXT NOT NULL,
+        preco DECIMAL NOT NULL,
+        data DATE NOT NULL,
+        id_produto TEXT NOT NULL,
+        FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+)''')
 
 print('Tabela criada com sucesso.')
 
