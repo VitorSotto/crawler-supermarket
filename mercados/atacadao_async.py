cbf78c04395b8f968d6cf3eadc4118ede250f854
Douglas Cardoso <douglas.dcm@gmail.com>
2023-07-06 02:32:08 -0300

    Experimenting the library Caqui to interact with the UI

--- mercados/atacadao_async.py
+++ mercados/atacadao_async.py
@@ -7,7 +7,7 @@ from datetime import date
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-# import pandas as pd
+import pandas as pd
 
 from mercados.mercado import Mercado
 
@@ -60,19 +60,28 @@ class CrawlerAtacadao(Mercado):
       inputsProducts = []
       inputsSelected = []
 
-      filterProducts = self.browser.find_element(By.ID, 'js-product-filter')
-      inputsProducts = filterProducts.find_elements(By.TAG_NAME, 'input')
+      # filterProducts = self.browser.find_element(By.ID, 'js-product-filter')
+      filterProducts = synchronous.find_element(*self.browser, By.ID, 'js-product-filter')
+      # inputsProducts = filterProducts.find_elements(By.TAG_NAME, 'input')
+      inputsProducts = synchronous.find_children_elements(*self.browser, filterProducts, By.TAG_NAME, 'input')
 
       for inputProduct in inputsProducts:
-        if (inputProduct.get_attribute('value').lower().find(self.searchProduct.lower())==0):
-          ActionChains(self.browser).scroll_to_element(inputProduct).perform()
-          ActionChains(self.browser).click(inputProduct).perform()
+        # value = inputProduct.get_attribute('value')
+        value = synchronous.get_attribute(*self.browser, inputProduct, 'value')
+        print("produto: ", value)
+        if (value.lower().find(self.searchProduct.lower())==0):
+          # ActionChains(self.browser).scroll_to_element(inputProduct).perform()
+          synchronous.actions_scroll_to_element(*self.browser, inputProduct)
+          # ActionChains(self.browser).click(inputProduct).perform()
+          synchronous.actions_click(*self.browser, inputProduct)
       
-      inputsSelected = filterProducts.find_elements(By.CSS_SELECTOR, 'input:checked')
+      # inputsSelected = filterProducts.find_elements(By.CSS_SELECTOR, 'input:checked')
+      inputsSelected = synchronous.find_children_elements(*self.browser, filterProducts, By.CSS_SELECTOR, 'input:checked')
       print(f'filtros selecionados: {numpy.size(inputsSelected)}')
       
       for inputSelected in inputsSelected:
-        print(inputSelected.get_attribute('value'))
+        # print(inputSelected.get_attribute('value'))
+        print(synchronous.get_attribute(*self.browser, inputSelected, 'value'))
         
       print('filtragem concluido!')
       print()
@@ -80,30 +89,50 @@ class CrawlerAtacadao(Mercado):
     def loading(self):
       print('carregando produtos...')
       
-      footer = self.browser.find_element(By.TAG_NAME, 'footer')
-      navFooter = footer.find_element(By.TAG_NAME, 'nav')
-      ActionChains(self.browser).scroll_to_element(navFooter).perform()
+      # footer = self.browser.find_element(By.TAG_NAME, 'footer')
+      footer = synchronous.find_element(*self.browser, By.TAG_NAME, 'footer')
+      # navFooter = footer.find_element(By.TAG_NAME, 'nav')
+      navFooter = synchronous.find_child_element(*self.browser, footer, By.TAG_NAME, 'nav')
+      # ActionChains(self.browser).scroll_to_element(navFooter).perform()
+      synchronous.actions_scroll_to_element(*self.browser, navFooter)
       sleep(1)
       
-      loadingDiv = self.browser.find_element(By.CLASS_NAME, 'container-catalogo')
-      loadingImg = loadingDiv.find_element(By.TAG_NAME, 'img')
-      ActionChains(self.browser).scroll_to_element(loadingDiv).perform()
+      # loadingDiv = self.browser.find_element(By.CLASS_NAME, 'container-catalogo')
+      loadingDiv = synchronous.find_element(*self.browser, By.CLASS_NAME, 'container-catalogo')
+      # loadingImg = loadingDiv.find_element(By.TAG_NAME, 'img')
+      loadingImg = synchronous.find_child_element(*self.browser, loadingDiv, By.TAG_NAME, 'img')
+      # ActionChains(self.browser).scroll_to_element(loadingDiv).perform()
+      synchronous.actions_scroll_to_element(*self.browser, loadingDiv)
       
-      catalogue = self.browser.find_element(By.CLASS_NAME, 'catalogue')
-      products = catalogue.find_elements(By.CLASS_NAME, 'product-box')
+      # catalogue = self.browser.find_element(By.CLASS_NAME, 'catalogue')
+      catalogue = synchronous.find_element(*self.browser, By.CLASS_NAME, 'catalogue')
+      # products = catalogue.find_elements(By.CLASS_NAME, 'product-box')
+      products = synchronous.find_child_element(*self.browser, catalogue, By.CLASS_NAME, 'product-box')
       # print(f'produtos encontrados: {numpy.size(products)}')
       
-      while(loadingImg.is_displayed()):
-        ActionChains(self.browser).scroll_to_element(loadingDiv).perform()
+      # while(loadingImg.is_displayed()):
+      product_list_start = synchronous.find_elements(
+        *self.browser,
+        locator_type="xpath",
+        locator_value="//div[contains(@class,'card shadow')]")
+      product_list_end = []
+      while(len(product_list_start) != len(product_list_end) ):
+        product_list_start = synchronous.find_elements(
+          *self.browser, locator_type="xpath", locator_value="//div[contains(@class,'card shadow')]")
+        # ActionChains(self.browser).scroll_to_element(loadingDiv).perform()
+        synchronous.actions_scroll_to_element(*self.browser, loadingDiv)
         sleep(5)
-        products = catalogue.find_elements(By.CLASS_NAME, 'product-box')
+        # products = catalogue.find_elements(By.CLASS_NAME, 'product-box')
+        products = synchronous.find_children_elements(*self.browser, catalogue, By.CLASS_NAME, 'product-box')
+        product_list_end = synchronous.find_elements(*self.browser, locator_type="xpath", locator_value="//div[contains(@class,'card shadow')]")
         
       print(f'produtos encontrados: {numpy.size(products)}')
       print('carregamento concluido!')
       print()
       
       for product in products:
-        ActionChains(self.browser).scroll_to_element(product).perform()
+        # ActionChains(self.browser).scroll_to_element(product).perform()
+        synchronous.actions_scroll_to_element(*self.browser, product)
 
     def getProduct(self):
       
@@ -111,7 +140,8 @@ class CrawlerAtacadao(Mercado):
       print()
       dataProduct = []
 
-      site = BeautifulSoup(self.browser.page_source, 'html.parser')
+      site = BeautifulSoup(
+        synchronous.get_page_source(*self.browser, 'html.parser'))
       products = site.find_all('div', attrs={'class': 'product-box'})
 
       for product in products:
@@ -128,12 +158,12 @@ class CrawlerAtacadao(Mercado):
         dataProduct.append([product_id, price_id, product_name, product_category, product_supplier, product_mercado, product_img, product_price, product_date])
       
       data = []
-      # data = pd.DataFrame(dataProduct, columns=['Id_Produto', 'Id_Preco', 'Nome', 'Categoria', 'Fornecedor', 'Mercado', 'Imagem', 'Preco', 'Data']).sort_values('Preco')
+      data = pd.DataFrame(dataProduct, columns=['Id_Produto', 'Id_Preco', 'Nome', 'Categoria', 'Fornecedor', 'Mercado', 'Imagem', 'Preco', 'Data']).sort_values('Preco')
       
       print('produtos pegos!')
       print()
       
-      # return pd.DataFrame(data.iloc[0]).transpose()
+      return pd.DataFrame(data.iloc[0]).transpose()
       return data
 
     def processa(self):
@@ -156,8 +186,8 @@ class CrawlerAtacadao(Mercado):
       print('configuração concluida!')
       print()
       
-      # products = pd.DataFrame(columns=['Id_Produto', 'Nome', 'Fornecedor', 'Mercado', 'Imagem', 'Id_Preco'])
-      # prices = pd.DataFrame(columns=['Id_Preco', 'Categoria', 'Preco', 'Data', 'Id_Produto'])
+      products = pd.DataFrame(columns=['Id_Produto', 'Nome', 'Fornecedor', 'Mercado', 'Imagem', 'Id_Preco'])
+      prices = pd.DataFrame(columns=['Id_Preco', 'Categoria', 'Preco', 'Data', 'Id_Produto'])
       res = []
 
       # self.browser.get('https://www.atacadao.com.br/')
@@ -177,8 +207,8 @@ class CrawlerAtacadao(Mercado):
 
       first_line = self.getProduct()
       
-      # products = pd.concat([products, first_line], join='inner', ignore_index=True)
-      # prices = pd.concat([prices, first_line], join='inner', ignore_index=True)
+      products = pd.concat([products, first_line], join='inner', ignore_index=True)
+      prices = pd.concat([prices, first_line], join='inner', ignore_index=True)
       
       print('Busca completa!')
       print()
@@ -186,7 +216,7 @@ class CrawlerAtacadao(Mercado):
       print('### ATACADAO CONCLUIDO! ###')
       print()
       
-      # res.append(products)
-      # res.append(prices)
+      res.append(products)
+      res.append(prices)
       
       return res
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
-# import pandas as pd
+import pandas as pd
 
 from mercados.atacadao import CrawlerAtacadao
 from mercados.supertonin import CrawlerSuperTonin
@@ -31,8 +31,8 @@ arquivo_abs_caminho = abs_pasta + '/' + arquivo
 with open(arquivo_abs_caminho, 'r') as arq:
   searchProducts = arq.read().split(',')
 
-# products = pd.DataFrame(columns=['Id_Produto', 'Nome', 'Fornecedor', 'Mercado', 'Imagem', 'Id_Preco'])
-# prices = pd.DataFrame(columns=['Id_Preco', 'Categoria', 'Preco', 'Data', 'Id_Produto'])
+products = pd.DataFrame(columns=['Id_Produto', 'Nome', 'Fornecedor', 'Mercado', 'Imagem', 'Id_Preco'])
+prices = pd.DataFrame(columns=['Id_Preco', 'Categoria', 'Preco', 'Data', 'Id_Produto'])
 
 print('=============================================================')
 print()
@@ -46,25 +46,25 @@ for searchProduct in searchProducts:
   browser = webdriver.Chrome(service=service,options=options)
 
   # Busca dos produtos no Atacacão
-  # crawlerAtacadao = CrawlerAtacadao(searchProduct, browser)
-  # crawlerAtacadaoRes = crawlerAtacadao.processa()
+  crawlerAtacadao = CrawlerAtacadao(searchProduct, browser)
+  crawlerAtacadaoRes = crawlerAtacadao.processa()
 
-  # products = pd.concat([products, crawlerAtacadaoRes[0]], ignore_index=True)
-  # prices = pd.concat([prices, crawlerAtacadaoRes[1]], ignore_index=True)
+  products = pd.concat([products, crawlerAtacadaoRes[0]], ignore_index=True)
+  prices = pd.concat([prices, crawlerAtacadaoRes[1]], ignore_index=True)
 
   # Busca dos produtos no SuperTonin
   crawlerSuperTonin = CrawlerSuperTonin(searchProduct, browser)
   crawlerSuperToninRes = crawlerSuperTonin.processa()
 
-  # products = pd.concat([products, crawlerSuperToninRes[0]], ignore_index=True)
-  # prices = pd.concat([prices, crawlerSuperToninRes[1]], ignore_index=True)
+  products = pd.concat([products, crawlerSuperToninRes[0]], ignore_index=True)
+  prices = pd.concat([prices, crawlerSuperToninRes[1]], ignore_index=True)
 
   # Busca dos produtos no Savegnago
   crawlerSavegnago = CrawlerSavegnago(searchProduct, browser)
   crawlerSavegnagoRes = crawlerSavegnago.processa()
 
-  # products = pd.concat([products, crawlerSavegnagoRes[0]], ignore_index=True)
-  # prices = pd.concat([prices, crawlerSavegnagoRes[1]], ignore_index=True)
+  products = pd.concat([products, crawlerSavegnagoRes[0]], ignore_index=True)
+  prices = pd.concat([prices, crawlerSavegnagoRes[1]], ignore_index=True)
 
   browser.close()
   print('Busca concluida!')
@@ -81,10 +81,10 @@ print()
 conn = sqlite3.connect("../server/prisma/dev.db")
 cursor = conn.cursor()
 
-# products.to_sql('products', conn, if_exists='append', index=False)
+products.to_sql('products', conn, if_exists='append', index=False)
 cursor.executemany('INSERT INTO Product (id, name, supplier, market, image, priceId) VALUES (?, ?, ?, ?, ?, ?)', products.to_numpy())
 
-# prices.to_sql('prices', conn, if_exists='append', index=False)
+prices.to_sql('prices', conn, if_exists='append', index=False)
 cursor.executemany('INSERT INTO Price (id, category, price, updatedAt, productId) VALUES (?, ?, ?, ?, ?)', prices.to_numpy())
 
 conn.commit()
